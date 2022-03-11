e4b62d6262bc49373acfbd4c20eae47786a7110a
mkostoevr <mkostoevr@yandex.ru>
2022-03-11 17:00:31 +0300

    [kernel] Remove memory manager

--- kernel/arch/x86/i686/main.c
+++ kernel/arch/x86/i686/main.c
@@ -6,8 +6,6 @@
 #include <kernel/libk/assert.h>
 #include <kernel/multiboot.h>
 #include <kernel/io/fdo.h>
-#include <kernel/mm/memlayout.h>
-#include <kernel/mm/physmem.h>
 #include <kernel/kernel.h>
 #include "gdt.h"
 #include "idt.h"
@@ -27,55 +25,6 @@ void arch_kmain(struct multiboot* mboot, u32 mboot_magic) {
 
     idt_install();
     printk_dup(FDO_ARCH_i686 "IDT installed\n");
-
-    printk_dup(FDO_ARCH_i686 "kernel_phys_start = %x\n" FDO_ARCH_i686 "kernel_phys_end = %x\n", KERNEL_START_PHYS, KERNEL_END_PHYS);
-
-    #define DONT_USE_UP_TO 0x100000
-
-    // parse memory map from grub
-    mboot_memmap_t *memmap = (mboot_memmap_t *)mboot->mmap_addr;
-    int memmap_entries_count = mboot->mmap_length / sizeof(mboot_memmap_t);
-    free_map_entry_t phys_free_map[memmap_entries_count];
-    int phys_free_map_len = 0;
-    size_t mem_total = 0;
-    for (int i = 0; i < memmap_entries_count; i++) {
-        if (memmap[i].type == MULTIBOOT_MEMORY_AVAILABLE) {
-            if (memmap[i].base_addr + memmap[i].length >= DONT_USE_UP_TO) {
-                phys_free_map[phys_free_map_len++] = (free_map_entry_t){(uintptr_t)memmap[i].base_addr, (size_t)memmap[i].length};
-            }
-        }
-        mem_total += (size_t)memmap[i].length;
-    }
-
-    uintptr_t bitmap_addr = 0;
-    printk_dup(FDO_ARCH_i686 "phys free map:\n");
-    for (int i = 0; i < phys_free_map_len; i++) {
-        printk_dup("    base = %x, len = %x\n", (u32)phys_free_map[i].addr, (u32)phys_free_map[i].length);
-        uintptr_t left_addr = phys_free_map[i].addr;
-        uintptr_t right_addr = left_addr + phys_free_map[i].length - 1;
-        if (left_addr <= KERNEL_START_PHYS && KERNEL_END_PHYS <= right_addr) {
-            if (right_addr - PAGE_ALIGN_UP(KERNEL_END_PHYS) + 1 >= mem_total / 8 ) { // mem_total/8 is phys mm bitmap size
-                bitmap_addr = PAGE_ALIGN_UP(KERNEL_END_PHYS);
-                break;
-            } else {
-                printk_dup("Cannot place phys bitmap after kernel. Halting...\n");
-                while (1) {}
-            }
-        }
-    }
-    printk_dup("\n");
-
-    physmem_init(bitmap_addr, phys_free_map, phys_free_map_len, mem_total);
-
-    /*while (1) {
-        char c = com1_read_char();
-        com1_write_char(c);
-        if (c == '\r') {
-            com1_write_char('\n');
-        } else if (c == 'q') {
-            break;
-        }
-    }*/
 
     kmain();
 }
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -3,7 +3,6 @@
 #include <kernel/types.h>
 #include <kernel/io/fdo.h>
 #include <kernel/io/termcolors.h>
-#include <kernel/mm/physmem.h>
 
 #ifdef __cplusplus
 extern "C" {
