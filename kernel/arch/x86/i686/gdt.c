5ec8e35f08cd6d07b88a3f00be683641c1978ff7
mkostoevr <mkostoevr@yandex.ru>
2022-03-14 18:04:17 +0300

    [kernel][x86] Replace access of GDT by bitfields

--- kernel/arch/x86/i686/gdt.c
+++ kernel/arch/x86/i686/gdt.c
@@ -1,6 +1,6 @@
 #include "gdt.h"
 
-void gdt_set_gate(u8 num, u64 base, u64 limit, u8 access, u8 gran) {
+void gdt_set_gate(u8 num, u64 base, u64 limit, u8 type, u8 s, u8 dpl, u8 p, u8 gran) {
     gdt.entries[num].base_low = (base & 0xFFFF);
     gdt.entries[num].base_middle = (base >> 16) & 0xFF;
     gdt.entries[num].base_high = (base >> 24) & 0xFF;
@@ -10,7 +10,10 @@ void gdt_set_gate(u8 num, u64 base, u64 limit, u8 access, u8 gran) {
 
     gdt.entries[num].granularity |= (gran & 0xF0);
 
-    gdt.entries[num].access = access;
+    gdt.entries[num].type = type;
+    gdt.entries[num].s = s;
+    gdt.entries[num].dpl = dpl;
+    gdt.entries[num].p = p;
 }
 
 void write_tss(int32_t num, u16 ss0, uint32_t esp0) {
@@ -19,7 +22,7 @@ void write_tss(int32_t num, u16 ss0, uint32_t esp0) {
     uintptr_t limit = base + sizeof *tss;
 
     // Add the TSS descriptor to the GDT
-    gdt_set_gate(num, base, limit, 0xE9, 0x00);
+    gdt_set_gate(num, base, limit, 0x9, 0, 3, 1, 0x00);
 
     memset(tss, 0x0, sizeof *tss);
 
@@ -40,13 +43,13 @@ void gdt_install() {
     gdtp->limit = sizeof gdt.entries - 1;
     gdtp->base = (uintptr_t)&gdt.entries[0];
 
-    gdt_set_gate(0, 0, 0, 0, 0);                // NULL segment
-    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
-    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
-    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User code
-    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User data
+    gdt_set_gate(0, 0, 0, 0, 0, 0, 0, 0);               // NULL segment
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0xA, 1, 0, 1, 0xCF); // Code segment
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x2, 1, 0, 1, 0xCF); // Data segment
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xA, 1, 3, 1, 0xCF); // User code
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0x2, 1, 3, 1, 0xCF); // User data
     write_tss(5, 0x10, 0x0);
-    gdt_set_gate(6, 0, 0xFFFFFFFF, 0xF2, 0xCF);
+    gdt_set_gate(6, 0, 0xFFFFFFFF, 0x2, 1, 3, 1, 0xCF);
 
     gdt_flush((uintptr_t)gdtp);
     tss_flush();
--- kernel/arch/x86/i686/gdt.h
+++ kernel/arch/x86/i686/gdt.h
@@ -7,7 +7,10 @@ typedef struct {
     u16 limit_low;
     u16 base_low;
     u8 base_middle;
-    u8 access;
+    u8 type: 4, // Segment type
+       s: 1,    // Descriptor type
+       dpl: 2,  // Descriptor privilige level (ring)
+       p: 1;    // Segment present
     u8 granularity;
     u8 base_high;
 } __attribute__((packed)) gdt_entry_t;
@@ -25,6 +28,6 @@ static struct {
 
 void gdt_flush(uintptr_t);
 
-void gdt_set_gate(u8 num, u64 base, u64 limit, u8 access, u8 gran);
+void gdt_set_gate(u8 num, u64 base, u64 limit, u8 type, u8 s, u8 dpl, u8 p, u8 gran);
 void write_tss(int32_t num, u16 ss0, uint32_t esp0);
 void gdt_install();
