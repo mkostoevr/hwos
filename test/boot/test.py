2786c66e73ec760c305d0dd945bb8552f2978496
Magomed Kostoev <mkostoevr@yandex.ru>
2022-10-23 13:24:20 +0300

    [test] Fix boot test

--- test/boot/test.py
+++ test/boot/test.py
@@ -6,7 +6,7 @@ import common
 
 def run():
     os = common.run_i686()
-    os.wait_for_debug_log("Hello, world!", timeout = 1)
-    os.wait_for_debug_log("Hello from generic_main", timeout = 0.1)
+    os.wait_for_debug_log("Hello, World!", timeout = 1)
+    os.wait_for_debug_log("Hello from kmain", timeout = 0.1)
     os.kill()
 
